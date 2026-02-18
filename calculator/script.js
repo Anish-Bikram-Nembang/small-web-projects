1ab7bfbacc86fc0c06ca5950ffea0879f64a0f61
Anish-Bikram-Nembang <anish.bikram.nembang@gmail.com>
2026-02-18 14:50:34 +0000

    wip: input parsing, try to optimize for floats too

--- calculator/script.js
+++ calculator/script.js
@@ -1,20 +1,15 @@
 var screen = document.getElementsByClassName("screen")[0];
-
+//main logic calculator function
 function calc() {
   const input = screen.value;
   screen.value = "";
   let nums = [];
-  for (let i = 0; i < input.length; i++) {
-    let currentDigit = parseFloat(input[i]);
-    if (!isNaN(currentDigit)) {
-      if (i == 0 || isNaN(parseFloat(input[i - 1]))) {
-        nums.push(currentDigit);
-      } else {
-        nums.push(nums.pop() * 10 + currentDigit);
-      }
-    } else {
-      nums.push(input[i]);
-    }
+  let inputLength = input.length;
+  while (inputLength > 0) {
+    //parsing floats and operators
+    let current = parseFloat(input[i]);
+    if (isNaN(current)) input.replace(current.toString(), "");
+    inputLength -= current.toString().length;
   }
   // nums = input.split(/[+/*%-]/);
   console.log(nums);
@@ -25,16 +20,22 @@ function calc() {
       outputQueue.push(nums[i]);
     } else {
       let operatorOnTop = 0;
+      operatorInInput = nums[i];
       while (operatorOnTop != undefined) {
-        operatorInInput = nums[i];
         operatorOnTop = operatorStack.pop();
-        if (precedence(operatorInInput) <= precedence(operatorOnTop)) {
+        if (precedence[operatorInInput] <= precedence[operatorOnTop]) {
+          outputQueue.push(operatorOnTop);
+          operatorStack.push(operatorInInput);
+        } else {
+          operatorStack.push(operatorOnTop);
+          operatorStack.push(operatorInInput);
+          break;
         }
       }
     }
   }
 }
-
+//function to add value of button pressed to input
 function addElement(value) {
   var newInput;
   if (typeof value != "string") {
@@ -44,11 +45,11 @@ function addElement(value) {
   }
   screen.value = newInput;
 }
-
+//utility function to clear screen
 function clearScreen() {
   screen.value = "";
 }
-
+//function for backspace button
 function deleteElement() {
   screen.value = screen.value.slice(0, -1);
 }
